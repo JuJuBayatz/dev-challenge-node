0325618b873b121088ef87834a9ab9b6e8895401
JuJuBayatz <laznimail1@gmail.com>
2020-04-11 01:27:23 +0200

    Delete token.helper.ts

--- src/token.helper.ts
+++
@@ -1,8 +0,0 @@
-import * as express from 'express';
-import * as jwt from 'jsonwebtoken';
-
-export function parseToken(request: express.Request): any {
-
-    const token = request.body.token || request.query.token || request.headers['authorization'];
-    return jwt.decode(token);
-};
